bae72299432a4387114b1498a394127982e1dc32
Samuel Schumacher <samschu@live.com>
2017-06-05 12:41:44 -0500

    Start writing basic user data call

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,33 @@
 const API_KEY = "XXXXXXXXXXXXXXXXXXXX";
 const USER_ID = "XXXXXXXXXXXXXXXXXXXX";
 
-alert(API_KEY);
+const USER_IMG_ELEMENT = document.getElementById("user-image");
+
+
+function updatePopup(userData) {
+  USER_IMG_ELEMENT.src = "https://habitica.com/export/avatar-:" + USER_ID + ".png";
+}
+
+function createApiRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+
+  if ("withCredentials" in xhr) {
+    xhr.open(method, url, true);
+  }
+  return xhr;
+}
+
+function makeApiRequest(url) {
+  var xhr = createApiRequest("GET", "https://allow-any-origin.appspot.com/https://habitica.com/api/v3/user?key=" + API_KEY + "&user=" + USER_ID);
+
+  xhr.onload = function() {
+    window.testData = JSON.parse(xhr.responseText);
+    updatePopup(JSON.parse(xhr.responseText));
+  };
+
+  xhr.onerror = function() {};
+
+  xhr.send();
+}
+
+makeApiRequest();
