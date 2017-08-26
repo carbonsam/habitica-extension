f184020014c70c2fdfbfcec53ef750584406e7dc
Samuel Schumacher <samschu@live.com>
2017-08-26 16:32:39 -0500

    Add blacklist option

--- src/js/options/index.js
+++ src/js/options/index.js
@@ -4,6 +4,8 @@ chrome
 
 const USER_ID_ELEMENT = document.getElementById('user_id_txt');
 const API_KEY_ELEMENT = document.getElementById('api_key_txt');
+const BLACKLIST_ELEMENT = document.getElementById('site_blacklist_txt');
+
 const SAVE_BTN_ELEMENT = document.getElementById('options_save_btn');
 const CLEAR_BTN_ELEMENT = document.getElementById('options_clear_btn');
 const STATUS_TXT_ELEMENT = document.getElementById('options_status_txt');
@@ -11,39 +13,44 @@ const STATUS_TXT_ELEMENT = document.getElementById('options_status_txt');
 const saveOptions = () => {
   const userIdText = USER_ID_ELEMENT.value;
   const apiKeyText = API_KEY_ELEMENT.value;
+  const blacklistText = BLACKLIST_ELEMENT.value;
 
   chrome.storage.local.set({
     'user_id': userIdText,
-    'api_key': apiKeyText
+    'api_key': apiKeyText,
+    'site_blacklist': blacklistText
   }, () => {
     STATUS_TXT_ELEMENT.textContent = 'Options saved successfully.';
 
     setTimeout(() => {
       STATUS_TXT_ELEMENT.textContent = '';
-    }, 1000);
+    }, 3000);
   });
 };
 
 const restoreOptions = () => {
   chrome.storage.local.get({
     'user_id': '',
-    'api_key': ''
+    'api_key': '',
+    'site_blacklist': ''
   }, function(items) {
     USER_ID_ELEMENT.value = items.user_id;
     API_KEY_ELEMENT.value = items.api_key;
+    BLACKLIST_ELEMENT.value = items.site_blacklist;
   });
 };
 
 const clearOptions = () => {
   chrome.storage.local.set({
     'user_id': '',
-    'api_key': ''
+    'api_key': '',
+    'site_blacklist': ''
   }, () => {
     STATUS_TXT_ELEMENT.textContent = 'Options cleared successfully.';
 
     setTimeout(() => {
       STATUS_TXT_ELEMENT.textContent = '';
-    }, 1000);
+    }, 3000);
 
     restoreOptions();
   });
